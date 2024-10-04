775fcd465293e2e43d79c68da6e9ad78df8f684c
bitcocom <bitcocom@empas.com>
2024-10-04 11:31:28 +0900

    post

---
+++ src/main/java/com/example/post/gpt/ChatClientConfig.java
@@ -0,0 +1,13 @@
+package com.example.post.gpt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ChatClientConfig {
+
+
+
+
+}
