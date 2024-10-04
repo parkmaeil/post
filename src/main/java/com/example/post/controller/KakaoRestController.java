c34f464181975fe35157b90652ca642094467d69
bitcocom <bitcocom@empas.com>
2024-10-04 09:51:56 +0900

    post

--- src/main/java/com/example/post/controller/KakaoRestController.java
+++ src/main/java/com/example/post/controller/KakaoRestController.java
@@ -12,7 +12,7 @@ public class KakaoRestController {
 
     private final String apiKey="fb1c310e228098321e6a6f5ef04723be";
     @GetMapping("/books")
-    public ResponseEntity<String> books(String title){
+    public ResponseEntity<?> books(String title){
         System.out.println(title); // ?
         final String url="https://dapi.kakao.com/v3/search/book?query="+title;
 
@@ -20,7 +20,7 @@ public class KakaoRestController {
         headers.set("Authorization", "KakaoAK "+apiKey);
         HttpEntity<String> entity=new HttpEntity<>(headers);
 
-        RestTemplate restTemplate=new RestTemplate();
+        RestTemplate restTemplate=new RestTemplate();                                        // JSON
         ResponseEntity<String> response=restTemplate.exchange(url, HttpMethod.GET, entity , String.class);
 
         return ResponseEntity
